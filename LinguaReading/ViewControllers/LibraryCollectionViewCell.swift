d075780b03d831e98d200c185e5c430771f8d53b
IrinaMuravyova <miomir@yandex.ru>
2024-05-16 20:19:03 +0800

    Added cover in model Book. Changed TableView to CollectionView

---
+++ LinguaReading/ViewControllers/LibraryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LibraryCollectionViewCell.swift
+//  LinguaReading
+//
+//  Created by Irina Muravyeva on 16.05.2024.
+//
+
+import UIKit
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+    
+}
